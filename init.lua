6bafa408f97c3e568d7bd6b0751125a47f886f40
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2025-06-25 14:10:28 -0700

    update vim shit

--- init.lua
+++ init.lua
@@ -937,11 +937,11 @@ require('lazy').setup({
             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
             group_index = 0,
           },
-          { name = 'copilot' },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
           { name = 'nvim_lsp_signature_help' },
+          { name = 'codeium' },
         },
       }
     end,
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,13 +8,27 @@ return {
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
   {
-    'zbirenbaum/copilot.lua',
+    'Exafunction/windsurf.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'hrsh7th/nvim-cmp',
+    },
     config = function()
-      require('copilot').setup {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      }
+      require('codeium').setup {}
     end,
   },
-  'zbirenbaum/copilot-cmp',
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+  },
 }
